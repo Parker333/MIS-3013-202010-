e0f850e46b6ffd85f90b2c1055299d36302a1792
Parker Hallows <Parker.A.Hallows-1@ou.edu>
2020-09-01 13:29:20 -0500

    Started to do Participation 1

--- Participations/Participation1_SumOf3Numbers/Program.cs
+++ Participations/Participation1_SumOf3Numbers/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
-namespace Participation1_SumOf3Numbers
+namespace SumOf3
 {
     class Program
     {
+        const double LUCKY_NUMBER = 7.777;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string numbner1 = "1";
+            string number2 = "5.5";
+            string number3 = "1.277";
+
+            Console.WriteLine("Please enter the number 1");
+            numbner1 = Console.ReadLine();
+
+            Console.WriteLine("Please enter the number 5.5");
+            number2 = Console.ReadLine();
+
+            Console.WriteLine("Please enter the number 2");
+            number3 = Console.ReadLine();
+
+            Console.WriteLine("What is the sum of the 3 number?");
+            string SumOf = Console.ReadLine();
+            double Sum = Convert.ToDouble(SumOf);
+
+
+
+            Console.WriteLine();
+            
+
+
+            
         }
     }
 }
