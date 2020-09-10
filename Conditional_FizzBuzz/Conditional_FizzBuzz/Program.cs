3e81b254ae933b0e4120e68660c8a7914e2411f6
Parker Hallows <Parker.A.Hallows-1@ou.edu>
2020-09-10 11:37:31 -0500

    Finished FizzBuzz

--- Conditional_FizzBuzz/Conditional_FizzBuzz/Program.cs
+++ Conditional_FizzBuzz/Conditional_FizzBuzz/Program.cs
@@ -8,47 +8,46 @@ namespace Conditional_FizzBuzz
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            int number = rnd.Next(1,101);
+            int number = rnd.Next(1, 101);
 
             //BOLEAN OPERATORS:
             //  OR ||
             //  AND &&
 
-            if (number % 3 == 0 && number % 5 == 0);
+            if (number % 3 == 0
+               && number % 5 == 0)
             {
-                Console.WriteLine("Fizz Buzz")
-
-
-                else if (number % 3 == 0) ;
-                {
-                    Console.WriteLine("Fizz")
-
-
-            else if (number % 5 == 0)
-                    {
-                        Console.WriteLine("Buzz");
-                    }
-
-                    else if (number % 3 != 0 || number % 5 != 0) ;
-                    {
-                        Console.WriteLine(number);
-                    }
-
-                    Console.WriteLine(number);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                }
+                Console.WriteLine("FizzBuzz");
             }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (number % 5 ==0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
