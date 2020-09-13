3c21bb414e78e8db728e73507d2ef6bf4b9be6eb
Parker Hallows <Parker.A.Hallows-1@ou.edu>
2020-09-13 15:04:49 -0500

    Finished Sale Total Homework

---
+++ Homework/Homework 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace Homework_1_Sale_Total
+{
+    class Program
+    {
+        const double Sales_Tax = 0.085;
+        static void Main(string[] args)
+        {
+            double quantity = 0;
+            double price = 0;
+            string productname = "Name";
+
+
+            Console.WriteLine("What is the product name of the item you are purchasing?");
+            productname = Console.ReadLine();
+
+            Console.WriteLine("How many" + " " + productname + "'s" + " " + "do you want to buy?");
+            quantity = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("What is the price for each" + " " + productname + "?");
+            price = Convert.ToDouble(Console.ReadLine());
+
+            double subtotal = (quantity * price);
+            double tax = (subtotal * Sales_Tax);
+            double total = (subtotal + tax);
+
+            Console.WriteLine("Your subtotal for your bill is " + subtotal.ToString("N2"));
+            Console.WriteLine("Your sales tax for your bill is " + tax.ToString("N2"));
+            Console.WriteLine("Your total for your bill is " + total.ToString("N2"));
+
+
+        }
+    }
+}
--- Participations/Conditional_FizzBuzz/Conditional_FizzBuzz/Program.cs
+++ Participations/Conditional_FizzBuzz/Conditional_FizzBuzz/Program.cs
@@ -51,3 +51,4 @@ namespace Conditional_FizzBuzz
 
 
 
+
