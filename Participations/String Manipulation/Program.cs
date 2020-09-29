519018ac0fb2e02d2ce56be0e96dd81f2fbcbd33
Parker Hallows <Parker.A.Hallows-1@ou.edu>
2020-09-29 12:01:37 -0500

    string manipulation finished

--- Participations/String Manipulation/Program.cs
+++ Participations/String Manipulation/Program.cs
@@ -9,23 +9,35 @@ namespace String_Manipulation
 {
     class Program
     {
+        static string programming = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
         static void Main(string[] args)
         {
-
-            string programming = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
 
             Console.WriteLine(programming);
             Console.WriteLine("");
 
             Console.WriteLine("What word do you want to find in the sentence above?");
-            string find = Convert.ToString(Console.ReadLine());
+            string find = Console.ReadLine();
 
             Console.WriteLine("What word do you want to change it to?");
-            string replace = Convert.ToString(Console.ReadLine());
+            string replace = Console.ReadLine();
 
+            if (programming.Contains(find) == true)
+            {
+                string newMessage = programming.Replace(find, replace);
+                Console.WriteLine(newMessage);
+            }
+            else
+            {
+                Console.WriteLine($"sorry, I couldn't find your word {find}.");
 
+                for (int i = find.Length - 1; i >= 0; i--)
+                {
+                    Console.Write(find[i]);
+                }
+            }
 
-
+            
 
             Console.ReadKey();
         }
