237ea569280a20e616d5b16e958d858a95c8eb53
Parker Hallows <Parker.A.Hallows-1@ou.edu>
2020-10-01 11:39:38 -0500

    started Collections 2

---
+++ InClassExamples/Collections 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> favoriteThings = new List<string>();
+            string answer;
+
+            do
+            {
+                Console.WriteLine("Please enter one of your favorite things");
+                string favoriteThing = Console.ReadLine();
+
+                favoriteThings.Add(favoriteThing);
+
+                Console.WriteLine("Do you have another favorite thing?");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower() == "yes");
+
+            Random rand = new Random();
+            int index = rand.Next(0, favoriteThings.Count);
+
+            Console.WriteLine(favoriteThings[index]);
+
+            foreach (var ft in favoriteThings)
+            {
+
+            }
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
