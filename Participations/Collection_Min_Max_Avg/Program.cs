4106b32252c9bf6c4992debe4838e0314fa27a49
Parker Hallows <Parker.A.Hallows-1@ou.edu>
2020-10-09 23:05:48 -0500

    "Finished" Collections Min/Max/Avg

---
+++ Participations/Collection_Min_Max_Avg/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_Min_Max_Avg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> ExamGrades = new List<double>();
+            string answer;
+            
+
+            do
+            {
+                Console.WriteLine("Please enter your exam grade.");
+                ExamGrades = Console.ReadLine();
+
+                ExamGrades.Add(ExamGrades);
+
+                Console.WriteLine("Do you have another exam grade?");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower() == "yes");
+
+            //I don't understand how this isn't working.Tried for a hot second but it's almost due so I'll stop here.
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
