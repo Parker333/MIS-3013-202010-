ae763e3101a23f24791ad98fb0c7a48456f110d4
Parker Hallows <Parker.A.Hallows-1@ou.edu>
2020-10-09 23:22:01 -0500

    "Finished" CSV Files

--- Participations/Processing CSV Files/Program.cs
+++ Participations/Processing CSV Files/Program.cs
@@ -11,6 +11,16 @@ namespace Processing_CSV_Files
     {
         static void Main(string[] args)
         {
+            string filepath = @"sales_data_sample.cvs";
+
+            string[] lines = File.ReadAllLines(filepath);
+
+            double sum = 0;
+            double sum2003 = 0;
+            double sum2004 = 0;
+            double sum2005 = 0;
+
+            // I looked at what you put on github and don't understand it. I'll have to go back and rewatch the lectures to get more undstanding.
     
 
 
