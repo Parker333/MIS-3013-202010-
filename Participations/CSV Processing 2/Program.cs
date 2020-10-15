b4ffb5516f8299721e57aa0c4857b11962c3c2e4
Parker Hallows <Parker.A.Hallows-1@ou.edu>
2020-10-15 17:27:52 -0500

    Finished CSV Processing 2

--- Participations/CSV Processing 2/Program.cs
+++ Participations/CSV Processing 2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,28 +12,27 @@ namespace CSV_Processing_2
     {
         static void Main(string[] args)
         {
-            var number = 1;
-            string userinput = "";
+            string filepath = @"C:\Users\Parker\source\repos\MIS-3013-202010-\Participations\CSV Processing 2\SalesJan2009.csv";
 
-            do
+            string[] lines = File.ReadAllLines(filepath);
+
+            //Transaction_date,Product,Price,Payment_Type,Name,City,State,Country,Account_Created,Last_Login,Latitude,Longitude
+            //Tran data = 0
+            //Visa = 3
+            //Name = 4
+            //USA = 7
+
+            for (int i = 1; i < lines.Length; i++)
             {
-                Console.WriteLine("Do you want to add to number?");
-                userinput = Console.ReadLine();
-                number++;
-
-            } while (userinput=="Yes");
-
-            Console.WriteLine(number);
-            
- 
-
-           
-
-
-
-
-
-
+                string line = lines[i];
+                string[] colums = line.Split(',');
+                if (colums[3] == "Visa" && colums[7] == "United States")
+                {
+                    Console.WriteLine(colums[4]);
+                    Console.WriteLine(colums[0]);
+                }
+            }
+       
             Console.ReadKey();
         }
     }
