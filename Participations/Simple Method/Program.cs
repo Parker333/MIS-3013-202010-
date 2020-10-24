532d11266aec8ade3a9529b539e417eee3be879a
Parker Hallows <Parker.A.Hallows-1@ou.edu>
2020-10-23 19:37:47 -0500

    Tried to do the methods participation

--- Participations/Simple Method/Program.cs
+++ Participations/Simple Method/Program.cs
@@ -10,16 +10,42 @@ namespace Simple_Method
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please choose a cat, dog, or cow.");
+            string animal = "";
 
+            do
+            {
+                   Console.WriteLine("Please choose a dog, monkey, or goat.");
+                    animal = Convert.ToString(Console.ReadLine());
 
+                if (animal == "dog")
+                {
+                    Console.WriteLine("A dog says woof woof");
+                    Console.WriteLine(" ");
 
+                }
+                else if (animal == "monkey")
+                {
+                    Console.WriteLine("A monkey says Oo Oo Ah Ah");
+                    Console.WriteLine(" ");
 
+                }
+                else if (animal == "goat")
+                {
+                    Console.WriteLine(" A goat says Bahhhh");
+                    Console.WriteLine(" ");
+                }
+                else
+                {
+                    Console.WriteLine("Please input one of the 3 animals");
+                }
 
-
+            } while ((animal != "dog" || animal != "monkey" || animal != "goat"));
+ 
 
 
             Console.ReadKey();
+           
+
         }
     }
 }
