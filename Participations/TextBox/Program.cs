d1d386580104d2349278070df74d41236fe02261
Parker Hallows <Parker.A.Hallows-1@ou.edu>
2020-11-30 22:28:19 -0600

    yeah

---
+++ Participations/TextBox/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //I don't really know what I'm supposed to be doing?
+            
+
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Participations/TextBox/Textbox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBox
+{
+    class Textbox
+    {
+        public int Padding { get; set; }
+        public ConsoleColor BackColor { get; set; }
+        public ConsoleColor FrontColor { get; set; }
+        
+
+        public void DisplayText()
+        {
+            string Text = "What am I even supposed to do?";
+
+        }
+
+    }
+}
